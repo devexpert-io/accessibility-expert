e572d01f1ea0b2e5417f1ea3392ce17368b4545c
antoniolg <antonio@devexperto.com>
2025-04-07 14:07:25 +0200

    5.3 Classic Views: Enable Accessibility checks in tests (base)

---
+++ app/src/androidTest/java/io/devexpert/accessibilityexpert/AccessibilityTest.kt
@@ -0,0 +1,27 @@
+package io.devexpert.accessibilityexpert
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import io.devexpert.accessibilityexpert.classic.exercise1.ContentDescriptionActivity
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Example of Espresso tests with accessibility.
+ */
+class AccessibilityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(ContentDescriptionActivity::class.java)
+
+    @Test
+    fun testContentDescriptions() {
+        onView(withId(R.id.btn_home))
+            .perform(click()) // AccessibilityChecks only run in ViewAction functions, like `click()`
+            .check(matches(isDisplayed()))
+    }
+} 

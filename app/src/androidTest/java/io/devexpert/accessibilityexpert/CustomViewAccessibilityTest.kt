3ba20b16c935be7649dfcecf97bf4c08656d313a
antoniolg <antonio@devexperto.com>
2025-04-07 15:27:40 +0200

    5.3 Classic Views: Custom View Accessibility tests (solution)

--- app/src/androidTest/java/io/devexpert/accessibilityexpert/CustomViewAccessibilityTest.kt
+++ app/src/androidTest/java/io/devexpert/accessibilityexpert/CustomViewAccessibilityTest.kt
@@ -1,9 +1,13 @@
 package io.devexpert.accessibilityexpert
 
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.accessibility.AccessibilityChecks
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isClickable
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isFocusable
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import io.devexpert.accessibilityexpert.classic.exercise8.CustomViewActivity
@@ -26,5 +30,16 @@ class CustomViewAccessibilityTest {
     fun testCustomViewAccessibilityProperties() {
         onView(withId(R.id.custom_view))
             .check(matches(isDisplayed()))
+            .check(matches(isClickable()))
+            .check(matches(isFocusable()))
+            .check(matches(withContentDescription(R.string.click)))
+            .check { view, _ ->
+                val nodeInfo = AccessibilityNodeInfoCompat.obtain()
+                view.onInitializeAccessibilityNodeInfo(nodeInfo.unwrap())
+
+                assert(nodeInfo.className == android.widget.Button::class.java.name) {
+                    "CustomView must behave like a button"
+                }
+            }
     }
 } 
